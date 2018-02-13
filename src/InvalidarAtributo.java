bb5677c7d6945eb7c6ab88e999b219754aafad6c
savonarola <g.mas.simon@gmail.com>
2018-02-13 17:09:18 +0100

    Invalidar sesión y duración de la sesión

--- src/InvalidarAtributo.java
+++ src/InvalidarAtributo.java
@@ -2,7 +2,6 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,66 +11,41 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/anyadir")
-public class AnyadirAtributo extends HttpServlet {
+@WebServlet("/invalidar")
+public class InvalidarAtributo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
-    public AnyadirAtributo() {
+    public InvalidarAtributo() {
         super();
     }
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//Reuperamos los dos atributos que nos manda el formulario
-		String atributo = request.getParameter("atributo");
-		String valor = request.getParameter("valor");
-		
 		HttpSession sesion = request.getSession();
 		
-		//Añadimos a la sesión la información del formulario
-		sesion.setAttribute(atributo, valor);
+		sesion.invalidate();
 		
-		//Salida del servlet para el usuario.
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
 		
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
 		out.println("<meta charset='utf-8'>");
-		out.println("<title> Titulo </title>");
+		out.println("<title> Sesion </title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Sesión del usuario </h1>");
-		
-		Enumeration<String> nombresDeAtributos = sesion.getAttributeNames();
-		
-		
-		while (nombresDeAtributos.hasMoreElements()) {
-			
-			 atributo =  nombresDeAtributos.nextElement();
-			 valor = (String) sesion.getAttribute(atributo);
-			 
-			 out.println("<p>Atributo: " + atributo + "</p>");
-			 out.println("<p>Valor: " + valor + "</p><hr>");
-			
-		}
-		
-		
-		out.println("<p> Contenido </p>");
+		out.println("<h1>Sesión Eliminada </h1>");
+		out.println("<p> Se ha borrado la sesion dle usuario </p>");
 		out.println("</body>");
 		out.println("</html>");
-
-
+		
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
 	}
 
 }
